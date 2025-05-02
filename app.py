3db655a66635c69dab81fd7af93eb64bce3d515d
Davis <charles.d.snead@gmail.com>
2025-05-01 21:06:26 -0400

    Added port forwarding

--- app.py
+++ app.py
@@ -33,5 +33,7 @@ def fetch_data():
     teams = [{'name': team['location'] + ' ' + team['nickname']} for team in data['teams']]
     return jsonify(teams)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render provides this env var
+    app.run(host='0.0.0.0', port=port)
